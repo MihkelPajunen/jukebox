1e8d7bccfea0f5b6efffa7032937af548fb1acf5
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-04-23 21:27:17 +0200

    added: 'animate on scroll' to AppCard component

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 
+// animate on scroll
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
 const app = createApp(App);
 
 app.use(router);
