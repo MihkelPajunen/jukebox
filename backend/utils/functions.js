85e8e5406f6e606b6d45584bf1c91b0aee47fe61
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-06-21 01:06:57 +0200

    added: titleize utility function

--- backend/utils/functions.js
+++ backend/utils/functions.js
@@ -3,4 +3,17 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-module.exports = { capitalize };
+function titleize(string) {
+  const exceptions = ['and', 'of', 'the'];
+
+  let words = string.split(' ');
+  words = words.map((element, index) => {
+    if (index === 0) return capitalize(element);
+    if (exceptions.includes(element.toLowerCase())) return element.toLowerCase();
+    return capitalize(element);
+  });
+
+  return words.join(' ');
+}
+
+module.exports = { capitalize, titleize };
