50fc91738eef83bd1f6f0de89b4f7838cc576d4c
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-06-22 13:40:12 +0200

    refactored: UploadView so that the entire request is processed on the back-end

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const busboy = require('busboy');
 const path = require('path');
 const os = require('os');
-const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { getArtist, createArtist } = require('./artists');
+const { getSpotifyTrack, getSpotifyArtist } = require('./spotify');
 
 router.post('/', (request, response) => {
   const bb = busboy({ headers: request.headers });
@@ -16,18 +18,24 @@ router.post('/', (request, response) => {
 
   const fileObject = {};
 
+  const errorResponse = (status, message = '') => {
+    request.unpipe(bb);
+    response.writeHead(status, { Connection: 'close' });
+    message.length > 0 ? response.end(message) : response.end();
+  };
+
   bb.on('file', (_name, file, info) => {
     fileObject['mimeType'] = info.mimeType;
 
     if (!['audio/flac', 'audio/x-flac'].includes(fileObject.mimeType)) {
-      request.unpipe(bb);
-      response.writeHead(415, { Connection: 'close' }).end('Unsupported file type');
+      errorResponse(415, 'Request includes an unsupported file type.');
     }
 
     fileObject['filePath'] = path.join(os.tmpdir(), `${uuidv4()}.flac`);
-    fileObject['fileSize'] = 0;
 
     const stream = fs.createWriteStream(fileObject.filePath);
+
+    fileObject['fileSize'] = 0;
 
     file.on('data', (data) => {
       stream.write(data);
@@ -37,12 +45,41 @@ router.post('/', (request, response) => {
     file.on('end', () => stream.close());
   });
 
-  bb.on('error', () => {
-    request.unpipe(bb);
-    response.writeHead(500, { Connection: 'close' }).end('Could not parse request');
-  });
+  bb.on('error', () => errorResponse(500, 'Request could not be parsed.'));
 
-  bb.on('close', () => response.status(200).json({ success: true }));
+  bb.on('close', async () => {
+    if (!fields?.artist || !fields?.title || !fields?.album) {
+      return errorResponse(422, 'Request did not specify all required fields.');
+    }
+
+    if (!fileObject) return errorResponse(422, 'Request did not contain a file.');
+
+    const spotifyTrack = await getSpotifyTrack(fields.artist, fields.title, fields.album);
+
+    const extras = {};
+
+    if (spotifyTrack) {
+      fields.title = spotifyTrack.title;
+      fields.album = spotifyTrack.album.title;
+      fields['imageUrl'] = spotifyTrack.album.imageUrl;
+
+      if (!(await getArtist(spotifyTrack.artist.name))) {
+        const spotifyArtist = await getSpotifyArtist(spotifyTrack.artist.id);
+
+        const artist = {
+          id: uuidv4(),
+          name: spotifyArtist.name,
+          imageUrl: spotifyArtist.imageUrl
+        };
+
+        createArtist(artist);
+        fields.artist = artist.id;
+        extras['artist'] = artist;
+      }
+    }
+
+    response.status(200).json({ success: true, ...extras });
+  });
 
   request.pipe(bb);
 });
--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -31,7 +31,7 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { useStoreNotifications } from '@/store/storeNotifications';
-import { artistExists, capitalize } from '@/utils/functions';
+import { titleize } from '@/utils/functions';
 import axios from 'axios';
 
 import AppInput from '@/components/AppInput.vue';
@@ -64,29 +64,6 @@ const formIsValid = () => {
   return true;
 };
 
-const getArtistImage = async (id: string) => {
-  try {
-    const response = await axios.get(`${import.meta.env.VITE_APP_API}/spotify/artists/${id}`);
-    return response.data?.artist?.imageUrl ?? '';
-  } catch {
-    return '';
-  }
-};
-
-const createArtist = async (artist: { name: string; imageUrl: string }) => {
-  const formData = new FormData();
-  artist.name && formData.append('name', artist.name);
-  artist.imageUrl && formData.append('imageUrl', artist.imageUrl);
-
-  try {
-    const response = await axios.post(`${import.meta.env.VITE_APP_API}/artists/new`, formData);
-    response.data.success && storeNotifications.add('is-success', 'New artist was created.');
-    return response.data?.artist ?? undefined;
-  } catch {
-    return undefined;
-  }
-};
-
 const clearForm = () => {
   form.value.artist = '';
   form.value.title = '';
@@ -104,42 +81,9 @@ const uploadFile = async () => {
   if (formIsValid()) {
     const formData = new FormData();
 
-    try {
-      let url = `${import.meta.env.VITE_APP_API}/spotify`;
-      url += `/artists/${form.value.artist}`;
-      url += `/tracks/${form.value.title}`;
-      url += `?album=${form.value.album}`;
-
-      const response = await axios.get(url);
-
-      if (response.data.success) {
-        formData.append('title', response.data.track.title);
-        formData.append('album', response.data.track.album.name);
-        formData.append('imageUrl', response.data.track.album.imageUrl);
-
-        if (await artistExists(response.data.track.artist.name || form.value.artist)) {
-          let url = `${import.meta.env.VITE_APP_API}/artists`;
-          url += `/${response.data.track.artist.name || form.value.artist}`;
-
-          const artist = await axios.get(url);
-          artist.data?.artist?.id && formData.append('artist', artist.data.artist.id);
-        } else {
-          const artist = await createArtist({
-            name: response.data.track.artist.name ?? form.value.artist,
-            imageUrl: await getArtistImage(response.data.track.artist.id)
-          });
-
-          artist.data?.artist?.id && formData.append('artist', artist.data.artist.id);
-        }
-      }
-    } catch {
-      console.log('Failed to retrieve track data from Spotify.');
-    }
-
-    formData.has('artist') || formData.append('artist', capitalize(form.value.artist));
-    formData.has('title') || formData.append('title', capitalize(form.value.title));
-    formData.has('album') || formData.append('album', capitalize(form.value.album));
-    formData.has('imageUrl') || formData.append('imageUrl', '');
+    formData.append('artist', titleize(form.value.artist));
+    formData.append('title', titleize(form.value.title));
+    formData.append('album', titleize(form.value.album));
     form.value.file && formData.append('file', form.value.file);
 
     controller = new AbortController();
@@ -152,7 +96,8 @@ const uploadFile = async () => {
     };
 
     try {
-      await axios.post(`${import.meta.env.VITE_APP_API}/upload`, formData, config);
+      const response = await axios.post(`${import.meta.env.VITE_APP_API}/upload`, formData, config);
+      response.data?.artist && storeNotifications.add('is-success', 'New artist was found.');
       storeNotifications.add('is-success', 'Upload has completed.');
     } catch (error) {
       if (axios.isAxiosError(error)) {
