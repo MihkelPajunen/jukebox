edc8aff69faf2539f8e41b36d87ad9d1a521b5d1
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-07-05 17:30:17 +0200

    added: AppSearch component

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="field has-addons">
+    <div class="control is-expanded">
+      <input class="input" type="text" :placeholder="placeholder" :disabled="disabled" />
+    </div>
+    <div class="control">
+      <a class="button is-info"><FontAwesome class="mr-2" icon="search" />Search</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  placeholder: {
+    type: String,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    required: true
+  }
+});
+</script>
