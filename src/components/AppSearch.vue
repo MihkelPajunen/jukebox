803b3d23e8850c62db27c0f82dc1b2e02178b8ba
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-07-09 18:31:59 +0200

    updated: AppSearch component to emit update:modelValue event

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="field has-addons">
     <div class="control is-expanded">
-      <input class="input" type="text" :placeholder="placeholder" :disabled="disabled" />
+      <input
+        @input="updateModelValue"
+        :value="modelValue"
+        class="input"
+        type="text"
+        :placeholder="placeholder"
+        :disabled="disabled"
+      />
     </div>
     <div class="control">
       <a class="button is-info"><FontAwesome class="mr-2" icon="search" />Search</a>
@@ -11,6 +18,9 @@
 
 <script setup lang="ts">
 defineProps({
+  modelValue: {
+    type: String
+  },
   placeholder: {
     type: String,
     required: true
@@ -20,4 +30,10 @@ defineProps({
     required: true
   }
 });
+
+const emit = defineEmits(['update:modelValue']);
+
+const updateModelValue = (event: Event) => {
+  emit('update:modelValue', (event.target as HTMLInputElement).value.trim());
+};
 </script>
