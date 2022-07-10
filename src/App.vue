746c51bb269b5cc7d1d6f6a6532720b5daf3947f
Mihkel Pajunen <mihkelpajunen@protonmail.com>
2022-07-10 03:38:18 +0200

    updated: App to include footer content

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
   <div id="content" class="container is-fullhd p-5">
     <RouterView />
   </div>
+  <footer class="footer p-4">
+    <div class="content has-text-centered">
+      <p>Created by <a target="_blank" :href="link"> Mihkel Pajunen</a> (2022).</p>
+    </div>
+  </footer>
   <AppNotifications />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import AppNavbar from '@/components/AppNavbar.vue';
 import AppNotifications from '@/components/AppNotifications.vue';
+
+const link = ref('https://github.com/MihkelPajunen');
 </script>
 
 <style lang="sass">
@@ -17,7 +25,7 @@ $navbar-breakpoint: 769px // collapse navbar when window width is less than 769p
 @import 'bulma/bulma'
 
 #content
-  min-height: calc(100vh - $navbar-height)
+  min-height: calc(100vh - $navbar-height - 56px)
 
 .is-fullhd
   max-width: 1514px !important
